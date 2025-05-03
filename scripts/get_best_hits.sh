a11530db366212a0bebf4c9baa2f9da2be73b8be
Martina Castellucci <martina.castellucci@studio.unibo.it>
2025-05-03 13:50:55 +0200

    Delete scripts/get_best_hits.sh

--- scripts/get_best_hits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Extract the best hit per query from a BLAST or HMMER tabular output file
-
-input_file=$1
-output_file=$2
-
-grep -v "^#" "$input_file" | sort -k1,1 -k5,5g | awk '!seen[$1]++' > "$output_file"
-
